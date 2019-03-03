f632406e020fe185bb04112ec42e9c507fedd39e
laishi <laishi@localhost.localdomain>
2019-03-03 18:05:43 +0800

    svg header

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + '/gald'));
+
+app.get('/dashboard', function(req, res) {
+    res.sendFile('/dashboard.html');
+});
+
+app.get('/', function(req, res) {
+    res.sendFile('/index.html');
+});
+app.get('/', function(req, res) {
+    res.redirect("/login.html");
+});
+
+
+app.listen(8080);
+
+console.log("Running at Port 8080");
